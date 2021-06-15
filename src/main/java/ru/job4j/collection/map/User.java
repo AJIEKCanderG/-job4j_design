7675fd531b5a45ed89444e4b4be206acab743d92
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-15 12:46:11 +0300

    4. Переопределить только equals [#1004]

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -14,9 +14,23 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children &&
+                name.equals(user.name) &&
+                birthday.equals(user.birthday);
+    }
+
+   /*    @Override
     public int hashCode() {
         return Objects.hash(name, children, birthday);
-    }
+    }*/
 
     public static void main(String[] args) {
             Map<User, Object> user = new HashMap<>();
