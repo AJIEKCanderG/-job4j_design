cd5b4e406f45f40da6bfd52bfe39dc381ce4bb91
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-23 16:04:04 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/MatrixFile.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MatrixFile {
+    public static String multiple(int size) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                builder.append((i + 1) * (j + 1));
+                if (j + 1 == size) {
+                    builder.append(System.lineSeparator());
+                } else {
+                    builder.append("  ");
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("C:/projects/job4j_design/Matrix.txt")) {
+            out.write(multiple(9).getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
