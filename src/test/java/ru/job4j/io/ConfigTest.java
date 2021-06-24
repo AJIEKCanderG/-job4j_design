d73b4b9d8425e40c910dfe3c5559d30860413c8c
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-24 14:48:05 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.net.URL;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        URL path = ClassLoader.getSystemResource("pair_without_comment.properties");
+        Config config = new Config(path.getPath());
+        config.load();
+        assertThat(config.value("name"),is("Petr Arsentev"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        URL path = ClassLoader.getSystemResource("pair_with_comment.properties");
+        Config config = new Config(path.getPath());
+        config.load();
+        assertThat(config.value("name"),is("Petr Arsentev"));
+    }
+}
