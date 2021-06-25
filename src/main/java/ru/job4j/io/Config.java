62e42bb461e848b13980d9f8528abb4dde50133a
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-25 17:58:51 +0300

    refactor

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -48,7 +48,7 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("./src/main/resources/app.properties"));
+        System.out.println(new Config("app.properties"));
     }
 
 }
