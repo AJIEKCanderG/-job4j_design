4d451a5ad65ec2bc4d45e984541a7ded3a6f62bd
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-25 08:34:56 +0300

    1. Читаем файл конфигурации [#858]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -48,7 +48,7 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("C:\\projects\\job4j_design\\src\\main\\resources\\app.properties"));
+        System.out.println(new Config("./src/main/resources/app.properties"));
     }
 
 }
