bdf2d639dbcc45eb5dfb8008daa6b383ab86b73e
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-06-28 00:52:24 +0300

    refactor

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
