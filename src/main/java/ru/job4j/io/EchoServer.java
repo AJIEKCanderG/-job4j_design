3a992f38d651553cb2b39a060f7591770f6f671b
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-07-19 00:07:38 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -7,21 +7,28 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            System.out.println("Сервер запущен!");
+            System.out.println("The server is running!");
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.contains("Bye")) {
+                    out.write("Hello, dear friend.".getBytes());
+                    String str = in.readLine();
+                    while (str != null && !str.isEmpty()) {
+                        if (str.startsWith("GET") && str.contains("Hello")
+                                || str.contains("hello")) {
+                            System.out.println("Hello!");
+                        } else if (str.contains("Exit") || str.contains("exit")) {
                             server.close();
-                            System.out.println("Сервер остановлен!");
+                            System.out.println("The server is stopped!");
+                        } else if (str.startsWith("GET") && !str.contains("Hello") && !str.contains("Exit")) {
+                               System.out.println("What? Repeat, please you command");
                         }
+                        str = in.readLine();
                     }
-                    out.flush();
+                   out.flush();
                 }
             }
         }
