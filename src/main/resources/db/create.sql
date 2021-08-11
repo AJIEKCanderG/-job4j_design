68279422d071fbd8ba7b13c0626f013676eddaf8
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-08-11 19:37:39 +0300

    1. Схема прав пользователи и роли. [#1093 #188395] refactor

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,9 +1,3 @@
-create table users (
-    id serial primary key,
-    name varchar(255),
-	role_id int references role(id)
-);
-
 create table role (
     id serial primary key,
     name varchar(255)
@@ -14,12 +8,27 @@ create table rules (
     name varchar(255)
 );
 
+create table users (
+    id serial primary key,
+    name varchar(255),
+	role_id int references role(id)
+);
+
 create table rolerules (
     id serial primary key,
 	role_id int references role(id),
 	rules_id int references rules(id)
 );
 
+create table category (
+    id serial primary key,
+    name varchar(255)
+);
+
+create table state (
+    id serial primary key,
+    name varchar(255)
+);
 
 create table item (
     id serial primary key,
