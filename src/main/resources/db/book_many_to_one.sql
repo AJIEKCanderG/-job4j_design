7b488fc14d3f9dc90d654f4f3a7b2922f82b40b1
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-08-12 23:00:37 +0300

    0.1. Связи между таблицами [#373615 #188318] refac

--- src/main/resources/db/book_many_to_one.sql
+++ src/main/resources/db/book_many_to_one.sql
@@ -1,19 +1,19 @@
-create table book(
-    id serial primary key,
-    name varchar(255)
-);
-
 create table author(
     id serial primary key,
     firstName varchar(255),
-	lastName varchar(255),
-	book_id int references book(id)
+	lastName varchar(255)	
 );
 
-insert into book(name) values ('Struktury dannykh i algoritmy');
-insert into book(name) values ('Filosofia_Java');
-insert into author(firstName, lastName, book_id) VALUES ('Robert', 'Lafore', 1);
-insert into author(firstName, lastName, book_id) VALUES ('Bryus', 'Ekkel', 2);
+create table book(
+    id serial primary key,
+    name varchar(255),
+	author_id int references author(id)
+);
+
+insert into author(firstName, lastName) VALUES ('Robert', 'Lafore');
+insert into author(firstName, lastName) VALUES ('Bryus', 'Ekkel');
+insert into book(name, author_id) values ('Struktury dannykh i algoritmy', 1);
+insert into book(name, author_id) values ('Filosofia_Java', 2);
 
 select * from book;
 select * from author where id in (select id from  book);
