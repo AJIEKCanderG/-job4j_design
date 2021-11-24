96c1f86588b9aa7349b5754982de90c0d318a458
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-11-24 22:54:06 +0300

    2. SQL Select by company [#2760] refacoring

--- src/main/resources/exam sql/company_with_max_count_persons.sql
+++ src/main/resources/exam sql/company_with_max_count_persons.sql
@@ -1,6 +1,6 @@
 select c.name as company, count(p.company_id) as max_person from person p join company c on c.id = p.company_id
 group by c.name
-having count(p.name) = (select  count(p.company_id)
-    as max_person from person p join company c on c.id = p.company_id
-group by c.name
+having count(p.name) = (select  count(company_id)
+    from person
+group by company_id
 order by 1 desc limit 1);
